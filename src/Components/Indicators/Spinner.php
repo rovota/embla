03ec86b970c2eb94cc2d5468fb4a32dab1b0cdf6
Léandro Tijink <leandro.tijink@gmail.com>
2023-12-24 18:47:51 +0100

    added: Indicators\Spinner component

---
+++ src/Components/Indicators/Spinner.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @author      Software Department <developers@rovota.com>
+ * @copyright   Copyright (c), Rovota
+ * @license     MIT
+ */
+
+namespace Rovota\Embla\Components\Indicators;
+
+use Rovota\Embla\Components\Component;
+
+class Spinner extends Component
+{
+
+	public function __construct()
+	{
+		parent::__construct('spinner');
+	}
+
+}
