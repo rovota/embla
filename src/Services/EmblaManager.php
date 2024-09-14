031044ae608baeebd6f0e6688b3bc55e57743c94
Léandro Tijink <leandro.tijink@gmail.com>
2024-09-14 21:52:08 +0200

    added: EmblaManager serviceprovider

---
+++ src/Services/EmblaManager.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @copyright   Léandro Tijink
+ * @license     MIT
+ */
+
+namespace Rovota\Embla\Services;
+
+use Rovota\Framework\Facades\View;
+use Rovota\Framework\Kernel\ServiceProvider;
+
+final class EmblaManager extends ServiceProvider
+{
+
+	// -----------------
+
+	public function __construct()
+	{
+		$this->attachAssets();
+	}
+
+	// -----------------
+
+	// -----------------
+
+	// -----------------
+
+	// -----------------
+
+	protected function attachAssets(): void
+	{
+		// JavaScript
+		View::attachScript('*', 'framework', [
+			'source' => '/vendor/rovota/embla/src/assets/scripts/framework.js',
+		]);
+
+		// CSS
+		View::attachLink('*', 'framework', [
+			'style' => '/vendor/rovota/embla/src/assets/styles/framework.css',
+		]);
+		View::attachLink('*', 'theming', [
+			'style' => '/vendor/rovota/embla/src/assets/styles/theming.css',
+		]);
+	}
+
+}
