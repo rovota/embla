2f6b99d158473db23571280450b36d904e4e0f40
Léandro Tijink <leandro.tijink@gmail.com>
2024-09-15 16:44:55 +0200

    added: InputComponent extension

---
+++ src/Base/Extensions/InputComponent.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @copyright   Léandro Tijink
+ * @license     MIT
+ */
+
+namespace Rovota\Embla\Base\Extensions;
+
+use Rovota\Embla\Base\Component;
+use Rovota\Embla\Utilities\Attributes\InputType;
+use Rovota\Framework\Facades\Cast;
+
+class InputComponent extends Component
+{
+
+	public function type(InputType|string $type): static
+	{
+		return $this->attribute('type', $type);
+	}
+
+	public function id(string|int $id): static
+	{
+		return $this->attribute('id', $id);
+	}
+
+	// -----------------
+	// Content
+
+	public function value(mixed $value): static
+	{
+		$value = Cast::toRawAutomatic($value);
+
+		if (strlen((string) $value) > 0) {
+			$this->attributeEscaped('value', $value);
+		}
+
+		return $this;
+	}
+
+	public function placeholder(string $placeholder): static
+	{
+		return $this->attributeTranslated('placeholder', $placeholder);
+	}
+
+	// -----------------
+	// Interactivity`
+
+	public function disabled(): static
+	{
+		return $this->attribute('disabled');
+	}
+
+	public function autoFocus(): static
+	{
+		return $this->attribute('autofocus');
+	}
+
+	// -----------------
+	// Constraints
+
+	public function required(): static
+	{
+		return $this->attribute('required');
+	}
+
+	public function readonly(): static
+	{
+		return $this->attribute('readonly');
+	}
+
+}
