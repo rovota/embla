7a79eb468eec4b4d4cb8aba653f1ada2947c5087
Léandro Tijink <leandro.tijink@gmail.com>
2024-09-15 16:44:42 +0200

    changed: overhauled Button component

---
+++ src/Components/Buttons/Button.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @copyright   Léandro Tijink
+ * @license     MIT
+ */
+
+namespace Rovota\Embla\Components\Buttons;
+
+use Rovota\Embla\Base\Component;
+
+class Button extends Component
+{
+
+	protected function configuration(): void
+	{
+		$this->config->tag = 'button';
+	}
+
+	// -----------------
+	// Starters
+
+	public static function name(string $name): static
+	{
+		return (new static)->attributes([
+			'name' => $name,
+			'type' => 'submit',
+		]);
+	}
+
+	// -----------------
+	// Content
+
+	public function value(mixed $value): static
+	{
+		if (strlen((string) $value) > 0) {
+			$this->attributeEscaped('value', $value);
+		}
+
+		return $this;
+	}
+
+	public function label(string $text): static
+	{
+		return $this->withTranslated($text);
+	}
+
+	// -----------------
+	// Interactivity
+
+	public function disabled(): static
+	{
+		$this->attribute('disabled');
+		return $this;
+	}
+
+	// -----------------
+	// Appearance
+
+	public function outlined(): static
+	{
+		return $this->class('outlined');
+	}
+
+	public function link(): static
+	{
+		return $this->class('link');
+	}
+
+}
