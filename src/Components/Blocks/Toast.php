42f84e24b381ab701e02697b6d438b6014896c3f
Léandro Tijink <leandro.tijink@gmail.com>
2024-09-20 14:30:59 +0200

    added: toast component

---
+++ src/Components/Blocks/Toast.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @copyright   Léandro Tijink
+ * @license     MIT
+ */
+
+namespace Rovota\Embla\Components\Blocks;
+
+use Rovota\Embla\Base\Component;
+use Rovota\Embla\Components\Layout\Content;
+use Rovota\Embla\Utilities\Colors\Status;
+use Rovota\Framework\Support\Str;
+
+class Toast extends Component
+{
+
+	protected function configuration(): void
+	{
+		$this->config->tag = 'toast';
+	}
+
+	// -----------------
+	// Starters
+
+	public static function createFrom(array $data): static
+	{
+		$component = new static;
+		$component->message($data['message'], $data['parameters'] ?? []);
+		$component->type($data['type'] ?? 'auto');
+
+		return $component;
+	}
+
+	// -----------------
+	// Content
+
+	public function message(string $text, array|object $data = []): static
+	{
+		return $this->attribute('data-message', Str::translate($text, $data));
+	}
+
+	// -----------------
+	// Content
+
+	public function type(mixed $type = 'auto'): static
+	{
+		return $this->attribute('data-type', $type);
+	}
+
+}
