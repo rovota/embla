285dc39850d931ecca0cd477b0ae8c1647d3b16f
Léandro Tijink <leandro.tijink@gmail.com>
2024-09-20 14:31:08 +0200

    added: anchor components

---
+++ src/Components/Navigation/Anchor.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * @copyright   Léandro Tijink
+ * @license     MIT
+ */
+
+namespace Rovota\Embla\Components\Navigation;
+
+use Rovota\Embla\Base\Component;
+use Rovota\Framework\Routing\UrlObject;
+use Rovota\Framework\Support\Url;
+
+class Anchor extends Component
+{
+
+	protected function configuration(): void
+	{
+		$this->config->tag = 'a';
+	}
+
+	// -----------------
+	// Starters
+
+	public static function toUrl(UrlObject $url): static
+	{
+		$component = new static;
+		$component->attribute('href', (string) $url);
+
+		return $component;
+	}
+
+	public static function to(string $path, array $parameters = []): static
+	{
+		return self::toUrl(Url::local($path, $parameters));
+	}
+
+	public static function toForeign(string $location, array $parameters = []): static
+	{
+		return self::toUrl(Url::foreign($location, $parameters));
+	}
+
+	public static function toRoute(string $name, array $context = [], array $parameters = []): static
+	{
+		return self::toUrl(Url::route($name, $context, $parameters));
+	}
+
+	/**
+	 * This method requires the presence of a cache store using the `session` driver.
+	 */
+	public static function toPrevious(string $default = '/'): static
+	{
+		return self::toUrl(Url::previous($default));
+	}
+
+	/**
+	 * This method requires the presence of a cache store using the `session` driver.
+	 */
+	public static function toNext(string $default = '/'): static
+	{
+		return self::toUrl(Url::next($default));
+	}
+
+	/**
+	 * This method requires the presence of a cache store using the `session` driver.
+	 */
+	public static function toIntended(string $default = '/'): static
+	{
+		return self::toUrl(Url::intended($default));
+	}
+
+	// -----------------
+	// Content
+
+	public function label(string $text, array|object $data = []): static
+	{
+		return $this->withTranslated($text, $data);
+	}
+
+	// -----------------
+	// Interactivity
+
+	public function asDownload(string $name = null): static
+	{
+		return $this->attribute('download', $name);
+	}
+
+	public function inNewTab(): static
+	{
+		return $this->attribute('target', '_blank');
+	}
+
+	public function withoutReferrer(): static
+	{
+		return $this->attribute('rel', 'noreferrer');
+	}
+
+}
---
+++ src/Components/Navigation/AnchorButton.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @copyright   Léandro Tijink
+ * @license     MIT
+ */
+
+namespace Rovota\Embla\Components\Navigation;
+
+class AnchorButton extends Anchor
+{
+
+	protected function configuration(): void
+	{
+		$this->config->tag = 'a';
+
+		$this->class('button');
+	}
+
+	// -----------------
+	// Content
+
+	public function icon(string $name): static
+	{
+		return $this->with(icon($name))->class('icon');
+	}
+
+	// -----------------
+	// Appearance
+
+	public function outlined(): static
+	{
+		return $this->class('outlined');
+	}
+
+	public function link(): static
+	{
+		return $this->class('link');
+	}
+
+}
