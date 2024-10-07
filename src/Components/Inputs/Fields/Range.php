3d010d59cf849c45832795de70b2e351ecba6ad6
Léandro Tijink <leandro.tijink@gmail.com>
2024-10-07 13:56:11 +0200

    added: revised Range component

---
+++ src/Components/Inputs/Fields/Range.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @copyright   Léandro Tijink
+ * @license     MIT
+ */
+
+namespace Rovota\Embla\Components\Inputs\Fields;
+
+use Rovota\Embla\Utilities\Attributes\InputType;
+
+class Range extends Base
+{
+
+	protected function configuration(): void
+	{
+		if ($this->attributes->missing('type')) {
+			$this->type(InputType::Range);
+		}
+	}
+
+	// -----------------
+	// Constraints
+
+	public function min(int|float $number): static
+	{
+		return $this->attribute('min', $number);
+	}
+
+	public function max(int|float $number): static
+	{
+		return $this->attribute('max', $number);
+	}
+
+	public function step(int|float $number): static
+	{
+		return $this->attribute('step', abs($number));
+	}
+
+}
