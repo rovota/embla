7264b371aaf40fd7931fe11f068376a45bd3521b
Léandro Tijink <leandro.tijink@gmail.com>
2024-12-23 16:15:28 +0100

    added: name and autocomplete attributes to LanguageSelect

--- src/Components/Inputs/Fields/Extensions/LanguageSelect.php
+++ src/Components/Inputs/Fields/Extensions/LanguageSelect.php
@@ -9,6 +9,7 @@ namespace Rovota\Embla\Components\Inputs\Fields\Extensions;
 
 use Rovota\Embla\Components\Inputs\Elements\Extensions\LanguageOption;
 use Rovota\Embla\Components\Inputs\Fields\Select;
+use Rovota\Embla\Utilities\Attributes\Autocomplete;
 use Rovota\Framework\Facades\Language;
 use Rovota\Framework\Localization\LanguageObject;
 
@@ -19,6 +20,9 @@ class LanguageSelect extends Select
 	{
 		$this->title('Change language');
 		$this->attribute('locale-switch');
+		
+		$this->name('language-switch');
+		$this->autocomplete(Autocomplete::Off);
 
 		$this->withForEach(Language::all(), function (LanguageObject $language) {
 			return LanguageOption::from($language);
