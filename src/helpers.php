87728f5f13fbec23f57079085e40ab837ae61dfa
Léandro Tijink <leandro.tijink@gmail.com>
2025-10-11 13:00:06 +0200

    removed: redundant helper method

--- src/helpers.php
+++ src/helpers.php
@@ -6,34 +6,8 @@
 
 use Carbon\Carbon;
 use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Support\Collection;
 use Rovota\Embla\Icons\Icon;
 use Rovota\Embla\Icons\IconManager;
-
-// -----------------
-// Templating Utilities
-
-if (!function_exists('array_to_classes')) {
-	function array_to_classes(Arrayable|array $data): string
-	{
-		if ($data instanceof Arrayable) {
-			$data = $data->toArray();
-		}
-
-		$classes = new Collection();
-
-		foreach ($data as $key => $value) {
-			if (is_numeric($key)) {
-				$classes->add($value);
-			}
-			if ($value === true) {
-				$classes->add((string)$key);
-			}
-		}
-
-		return trim($classes->join(' '));
-	}
-}
 
 // -----------------
 // Content
