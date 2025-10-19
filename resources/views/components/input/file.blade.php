732f4467c3079ed288eaea47904a2198e18268fd
Léandro Tijink <leandro.tijink@gmail.com>
2025-10-19 11:02:46 +0200

    fixed: missing translation calls for file input placeholders

--- resources/views/components/input/file.blade.php
+++ resources/views/components/input/file.blade.php
@@ -1,11 +1,11 @@
-@props(['missing' => 'Select file(s)', 'selected' => '%1$s files selected', ''])
+@props(['missing' => 'Select file(s)', 'selected' => '%1$s selected'])
 @aware(['name' => null])
 
 <input type="file" {{ $attributes->merge([
 	'id' => $name,
 	'name' => $name,
-	'data-missingcaption' => $missing,
-	'data-selectedcaption' => $selected,
+	'data-missingcaption' => __($missing),
+	'data-selectedcaption' => __($selected),
 ]) }}>
 
 <label for="{{ $name }}"></label>
