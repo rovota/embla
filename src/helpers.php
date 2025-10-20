e5321bfb2d095dd0999630071634a8ca06d1781f
Léandro Tijink <leandro.tijink@gmail.com>
2025-10-20 17:02:20 +0200

    fixed: re-introduced moment() helper function.

--- src/helpers.php
+++ src/helpers.php
@@ -5,6 +5,7 @@
  * @license     MIT
  */
 
+use Illuminate\Support\Carbon;
 use Rovota\Embla\Icons\Icon;
 use Rovota\Embla\Icons\IconManager;
 
@@ -15,5 +16,12 @@ if (!function_exists('icon')) {
 	function icon(string $name): Icon|null|string
 	{
 		return app(IconManager::class)->get($name);
+	}
+}
+
+if (!function_exists('moment')) {
+	function moment(mixed $value, DateTimeZone|string|int|null $timezone = null): Carbon
+	{
+		return new Carbon($value, $timezone);
 	}
 }
