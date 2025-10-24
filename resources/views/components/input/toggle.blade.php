4563f1b77ecacf5f549917b3249f83532eb7cd56
Léandro Tijink <leandro.tijink@gmail.com>
2025-10-24 19:51:37 +0200

    added: ability to define multiple active checkboxes within the same name group

--- resources/views/components/input/toggle.blade.php
+++ resources/views/components/input/toggle.blade.php
@@ -1,11 +1,16 @@
 @aware(['type' => 'checkbox', 'name' => null, 'default' => null, 'fallback' => 'on'])
 @props(['label', 'description', 'value' => '1'])
 
+@php
+	$defaults = old($value, str_contains($default, '|') ? old($name, explode('|', $default)) : [$default]);
+	$checked = in_array($value, $defaults);
+@endphp
+
 <label {{ $attributes->class(['input-toggle', 'masked'])->only(['class'])->merge(['aria-label' => __($label)]) }}>
 	@if($fallback === 'on')
 		<input type="hidden" name="{{ $name }}" value="0">
 	@endif
-	<input type="{{ $type }}" {{ $attributes->merge(['id' => $value, 'name' => $name, 'value' => $value])->except('class') }} @checked(old($name, $default) === $value)>
+	<input type="{{ $type }}" {{ $attributes->merge(['id' => $value, 'name' => $name, 'value' => $value])->except('class') }} @checked($checked)>
 	<content>
 		<span>{{ __($label) }}</span>
 		@isset($description)
