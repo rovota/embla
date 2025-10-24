c4a8a0a8ba68881b4ae601ed8014a977fcd7c02c
Léandro Tijink <leandro.tijink@gmail.com>
2025-10-24 19:22:23 +0200

    added: id property to checkbox and toggle components with the input value

--- resources/views/components/input/toggle.blade.php
+++ resources/views/components/input/toggle.blade.php
@@ -5,7 +5,7 @@
 	@if($fallback === 'on')
 		<input type="hidden" name="{{ $name }}" value="0">
 	@endif
-	<input type="{{ $type }}" {{ $attributes->merge(['name' => $name, 'value' => $value])->except('class') }} @checked(old($name, $default) === $value)>
+	<input type="{{ $type }}" {{ $attributes->merge(['id' => $value, 'name' => $name, 'value' => $value])->except('class') }} @checked(old($name, $default) === $value)>
 	<content>
 		<span>{{ __($label) }}</span>
 		@isset($description)
