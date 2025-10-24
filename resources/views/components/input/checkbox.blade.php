0a4da461c66011922428282b148aa6c55ab81d79
Léandro Tijink <leandro.tijink@gmail.com>
2025-10-24 19:05:52 +0200

    added: ability to disable toggle/checkbox fallback input

--- resources/views/components/input/checkbox.blade.php
+++ resources/views/components/input/checkbox.blade.php
@@ -1,8 +1,10 @@
-@aware(['type' => 'checkbox', 'name' => null, 'default' => null])
+@aware(['type' => 'checkbox', 'name' => null, 'default' => null, 'fallback' => 'on'])
 @props(['label', 'value' => '1'])
 
 <label {{ $attributes->class(['input-choice', 'masked'])->only(['class'])->merge(['aria-label' => __($label)]) }}>
-	<input type="hidden" name="{{ $name }}" value="0">
+	@if($fallback === 'on')
+		<input type="hidden" name="{{ $name }}" value="0">
+	@endif
 	<input type="{{ $type }}" {{ $attributes->merge(['name' => $name, 'value' => $value])->except('class') }} @checked(old($name, $default) === $value)>
 	<indicator></indicator>
 	<content>
